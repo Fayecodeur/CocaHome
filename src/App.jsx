008d6a7bd319763276e9aefbeb8e9bb27d8a5961
Fayecodeur <fayecodeur99@gmail.com>
2025-01-10 18:05:16 +0000

    ajout des composants

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
-
+import Content from "./components/Content.jsx";
+import ToggleLang from "./components/ToggleLang.jsx";
+import ContextProvider from "./context/langContex.jsx";
 function App() {
-  return <></>;
+  return (
+    <>
+      <ContextProvider>
+        <ToggleLang />
+        <Content />
+      </ContextProvider>
+    </>
+  );
 }
 
 export default App;
